3e26aff7567aebceeda04525dac9da80a707eeb8
AshKemp <kmuraliashwin@gmail.com>
2023-03-25 19:55:31 +0530

    add comments on important features of parcel

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
+/**
+ * Creater server
+ * HMR - Hot Module Replacement
+ * File Watcher Algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Cleaning our code
+ * Dev and Production Build
+ * Super fast build algorithm
+ * Image Optimization
+ * Caching while development
+ * Compression
+ * Compatible with older version of browser
+ * HTTPS on dev
+ * port number
+ * Consistent hashing algorithm
+ * Zero config
+ * Tree Shaking - Removing unwanted code
+ *
+ * Transitive dependencies
+ */
 const heading = React.createElement(
   "h1",
   { id: "title" },
